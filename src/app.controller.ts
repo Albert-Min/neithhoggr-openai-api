715422867e4d67c11be23688cfff13c3283c7f9f
Albert Min <sixiang.min@rea-group.com>
2023-06-09 15:31:18 +0800

    feat: introduce swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { OpenAIService } from './openai.service';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly openaiService: OpenAIService) {}
 
   @Get('/openai')
-  async getOpenAIResponse(): Promise<string> {
-    const prompt = 'Hello, OpenAI!';
+  @ApiResponse({
+    status: 200,
+    description: 'Response from openai api',
+    type: String,
+  })
+  async getOpenAIResponse(@Param('prompt') prompt: string): Promise<string> {
     return await this.openaiService.callOpenAPI(prompt);
   }
 
   @Get('/heartbeat')
+  @ApiResponse({
+    status: 200,
+    description: 'OK',
+    type: String,
+  })
   getHeartbeat(): string {
     return 'OK';
   }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './openai.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Neithhoggr')
+    .setDescription('Neithhoggr OpenAI API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(port, () => {
+    console.log(`Application is running on: http://localhost:${port}`);
+  });
 }
 
 bootstrap();
