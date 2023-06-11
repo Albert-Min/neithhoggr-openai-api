ff77682af4ab5240b7f486a061e71971b7359496
Albert Min <sixiang.min@rea-group.com>
2023-06-11 22:42:19 +0800

    feat: use fastify and enable cors and csrf

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
+import fastifyCsrf from '@fastify/csrf-protection';
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { PORT } from './environment';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Neithhoggr')
@@ -18,6 +25,13 @@ async function bootstrap() {
   SwaggerModule.setup('swagger', app, document);
 
   app.enableShutdownHooks();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+  await app.register(fastifyCsrf);
   await app.listen(PORT, '0.0.0.0');
 
   console.log(`Application is running on: ${await app.getUrl()}`);
