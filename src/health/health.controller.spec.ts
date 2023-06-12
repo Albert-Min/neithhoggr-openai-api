93bc87d1c777c52386fc055921909174152171b3
Albert Min <sixiang.min@rea-group.com>
2023-06-12 22:45:04 +0800

    fix: add lint and unit test

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -1,19 +1,57 @@
+import { HttpModule } from '@nestjs/axios';
+import {
+  HealthCheckError,
+  HealthCheckResult,
+  HealthCheckService,
+  TerminusModule,
+} from '@nestjs/terminus';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { HealthController } from './health.controller';
 
 describe('HealthController', () => {
   let controller: HealthController;
+  let healthCheckService: HealthCheckService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TerminusModule, HttpModule],
       controllers: [HealthController],
     }).compile();
 
     controller = module.get<HealthController>(HealthController);
+    healthCheckService = module.get<HealthCheckService>(HealthCheckService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('check', () => {
+    it('should return health check result', async () => {
+      // Mock the health check result
+      const healthCheckResult: HealthCheckResult = {
+        info: {
+          nestjsDocs: { status: 'up' },
+        },
+        status: 'ok',
+        details: {},
+      };
+      jest
+        .spyOn(healthCheckService, 'check')
+        .mockResolvedValue(healthCheckResult);
+
+      const result = await controller.check();
+
+      expect(result).toEqual(healthCheckResult);
+    });
+
+    it('should throw HealthCheckError if check fails', async () => {
+      // Mock the health check failure
+      const healthCheckResult = { nestjsDocs: { status: 'down' } };
+      jest
+        .spyOn(healthCheckService, 'check')
+        .mockRejectedValue(
+          new HealthCheckError('Health check failed', healthCheckResult),
+        );
+
+      await expect(controller.check()).rejects.toThrowError(HealthCheckError);
+    });
   });
 });
