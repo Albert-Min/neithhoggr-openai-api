085343c356e6a04ac530583c344fdd6e5c87e8b0
Albert Min <sixiang.min@rea-group.com>
2023-06-16 16:50:44 +0800

    chore: remove unnessary imports

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,13 +1,11 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { TerminusModule } from '@nestjs/terminus';
-import { MONGO_URL } from 'src/environment';
 
 import { HealthController } from './health.controller';
 
 @Module({
-  imports: [TerminusModule, HttpModule, MongooseModule.forRoot(MONGO_URL)],
+  imports: [TerminusModule, HttpModule],
   controllers: [HealthController],
 })
 export class HealthModule {}
